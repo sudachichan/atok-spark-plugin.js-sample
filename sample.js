e08adeaef91e85cb8ed486e7064b5dfeeebfdea1
sudachichan <sudachichan@users.noreply.github.com>
2016-09-09 21:34:11 +0900

    サンプルを簡易プラグイン記法を使用したものに書き換え。

--- sample.js
+++ sample.js
@@ -1,20 +1,14 @@
 var Plugin = require('atokspark-jsplugin');
 
-var commands = [
-    ['pluginjs:', 'node.jsサンプルです'],
-    ['pluginjs2:', '=============\n  ATOK Spark + node.js Plugin\n============='],
-];
-
-var sample = new Plugin().run();
-sample.on('check', function (text, callback) {
-    var found = -1;
-    commands.forEach(function (command, i) {
-        if (command[0] === text) {
-            found = i;
-        }
-    });
-    callback(found > -1 ? found : null);
-});
-sample.on('gettext', function (token, callback) {
-    callback(commands[token][1]);
+// {正規表現: 関数} の連想配列を指定することで、簡単にプラグインを定義できます。
+Plugin.byRules({
+    'pluginjs:': function () {
+        return 'node.jsサンプルです';
+    },
+    'pluginjs2:': function () {
+        return '=============\n  ATOK Spark + node.js Plugin\n=============';
+    },
+    'pluginjs3:(.*):': function (matches) {
+        return '正規表現を使う場合のサンプルです。「' + matches[1] + '」にマッチしました。'; 
+    },
 });
