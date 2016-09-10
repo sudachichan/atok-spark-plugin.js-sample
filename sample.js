929f53fd9838c0c6f0dcb22b8d2ef6f6fcb9f659
sudachichan <sudachichan@users.noreply.github.com>
2016-09-11 03:59:46 +0900

    プラグインフレームワークの仕様変更に対応

--- sample.js
+++ sample.js
@@ -2,13 +2,20 @@ var Plugin = require('atokspark-jsplugin');
 
 // {正規表現: 関数} の連想配列を指定することで、簡単にプラグインを定義できます。
 Plugin.byRules({
-    'pluginjs:': function () {
-        return 'node.jsサンプルです';
+    replaces: {
+        'pluginjs:': function () {
+            return 'node.jsサンプルです';
+        },
+        'pluginjs2:': function () {
+            return '=============\n  ATOK Spark + node.js Plugin\n=============';
+        },
+        'pluginjs3:(.*):': function (matches) {
+            return '正規表現を使う場合のサンプルです。「' + matches[1] + '」にマッチしました。'; 
+        },
     },
-    'pluginjs2:': function () {
-        return '=============\n  ATOK Spark + node.js Plugin\n=============';
-    },
-    'pluginjs3:(.*):': function (matches) {
-        return '正規表現を使う場合のサンプルです。「' + matches[1] + '」にマッチしました。'; 
+    views: {
+        'pluginjs:help:': function () {
+            return '<div>hoge</div>';
+        },
     },
 });
