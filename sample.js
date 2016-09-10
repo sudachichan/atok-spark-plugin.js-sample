a5ba55b761afe0d2d9207bed4894417388cbce7f
sudachichan <sudachichan@users.noreply.github.com>
2016-09-11 06:06:27 +0900

    テンプレート文字列リテラルを活用

--- sample.js
+++ sample.js
@@ -19,14 +19,14 @@ Plugin.byRules({
             return '=============\n  ATOK Spark + node.js Plugin\n=============';
         },
         'pluginjs3:(.*):': (matches) => {
-            return '正規表現を使う場合のサンプルです。「' + matches[1] + '」にマッチしました。'; 
+            return `正規表現を使う場合のサンプルです。「${matches[1]}」にマッチしました。`; 
         },
     },
     // views: {正規表現: 関数} を指定すると、
     // 正規表現にマッチしたトリガーで、戻り値のXHTMLを表示します。
     views: {
         'pluginjs:help:': () => {
-            const README_md = fs.readFileSync(__dirname + '/README.md', 'utf8'); 
+            const README_md = fs.readFileSync(`${__dirname}/README.md`, 'utf8'); 
             return marked(README_md);
         },
     },
